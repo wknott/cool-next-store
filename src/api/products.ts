59cbf8f58390dc493481d56696b69f2284ea9333
Wojciech Knott <wojciechknott@gmail.com>
2024-03-19 22:34:22 +0100

    Implemented getProductList and getProductById api methods

---
+++ src/api/products.ts
@@ -0,0 +1,54 @@
+import { type ProductItemType } from "@/ui/types";
+const API_URL = "https://naszsklep-api.vercel.app/api/products";
+const NUMBER_OF_PRODUCTS_ON_PAGE = 20;
+
+type ProductResponseItem = {
+	id: string;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	rating: {
+		rate: number;
+		count: number;
+	};
+	image: string;
+	longDescription: string;
+};
+
+export const getProductList = async (pageNumber = 0) => {
+	const params = new URLSearchParams();
+	params.append("take", String(NUMBER_OF_PRODUCTS_ON_PAGE));
+
+	if (pageNumber > 0) {
+		params.append("offset", String(pageNumber * NUMBER_OF_PRODUCTS_ON_PAGE));
+	}
+
+	const url = `${API_URL}?${params.toString()}`;
+
+	const res = await fetch(url);
+	const productsResponse = (await res.json()) as ProductResponseItem[];
+
+	const products = productsResponse.map(productResponseItemToProductItemType);
+
+	return products;
+};
+
+export const getProductById = async (id: ProductResponseItem["id"]) => {
+	const response = await fetch(`${API_URL}/${id}`);
+	const product = (await response.json()) as ProductResponseItem;
+
+	return productResponseItemToProductItemType(product);
+};
+
+const productResponseItemToProductItemType = (product: ProductResponseItem): ProductItemType => ({
+	id: product.id,
+	name: product.title,
+	category: product.category,
+	price: product.price,
+	description: product.description,
+	image: {
+		src: product.image,
+		alt: product.title,
+	},
+});
